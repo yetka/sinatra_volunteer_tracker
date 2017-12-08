5d1851981085720f863cb4c048864e13d0c7a701
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-08 10:28:55 -0800

    add .all method to Project class

--- lib/project.rb
+++ lib/project.rb
@@ -6,3 +6,14 @@ class Project
     @id = attributes.fetch(:id)
   end
 end
+
+def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      name = project.fetch("name")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:name => name, :id => id}))
+    end
+    projects
+  end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,10 +1,30 @@
 require('spec_helper')
 
 describe(Project) do
+
   describe("#initialize") do
     it("creates a new project object") do
       new_project = Project.new({:name => "project1", :id => nil})
       expect(new_project).to(be_an_instance_of(Project))
     end
   end
+
+  describe("#name") do
+      it("tells you its name") do
+        new_project = Project.new({:name => "project1", :id => nil})
+        expect(new_project.name()).to(eq("project1"))
+      end
+    end
+
+    def self.all
+      returned_projects = DB.exec("SELECT * FROM projects;")
+      projects = []
+      returned_projects.each do |project|
+        name = project.fetch("name")
+        id = project.fetch("id").to_i
+        projects.push(Project.new({:name => name, :id => id}))
+      end
+      projects
+    end
+
 end
