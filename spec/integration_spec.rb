71312e402c74cab7170120577037cd47e81c34b1
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-12-08 14:43:08 -0800

    add integration tests to see volunteer and project forms

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -10,3 +10,19 @@ describe("users viewing all projects", {:type => :feature}) do
     expect(page).to have_content('Your Current Projects')
   end
 end
+
+describe("users viewing label for new project", {:type => :feature}) do
+  it("allows an user to add new project to database") do
+    visit('/')
+    click_link('Click here to add new Project')
+    expect(page).to have_content('Add New Project')
+  end
+end
+
+describe("users viewing label for new volunteer", {:type => :feature}) do
+  it("allows an user to add new volunteer to database") do
+    visit('/')
+    click_link('Click here to add new Volunteer to a Project')
+    expect(page).to have_content('Add Volunteer to any Project:')
+  end
+end
